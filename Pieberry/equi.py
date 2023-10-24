fce0dfabfc686a32d9c2a8a3b8321154d8d0949a
Tzheng <Tzheng456@gmail.com>
2023-10-24 09:47:09 -0700

    asdfsdaf

--- Pieberry/equi.py
+++ Pieberry/equi.py
@@ -3,27 +3,37 @@ import pydirectinput
 import threading
 import keyboard
 import sys
+import random
 
 from assets import *
 
 running = True
+sleep_time = 20.52
 
 def search(asset, conf=0.9):
     return pyautogui.locateOnWindow(asset, "YEONHWA", confidence=conf)
 
 def search_image():
+    print(f"auto equilibrium glitcher started")
     while running:
+        global sleep_time
         try:
             # Locate the image in the specified region of the screen
             x = search(EQUILIBRIUM)
             if x is not None:
-                print(f"ENTERED EQUILIBRIUM, COUNTDOWN THEN CAST DOOR")
-                pyautogui.sleep(20.48)
+                print(f"ENTERED EQUILIBRIUM, COUNTDOWN: {sleep_time} THEN CAST DOOR")
+                pyautogui.sleep(sleep_time)
+                print(f"CASTING DOOR NOW!")
                 pydirectinput.press("n")
+                print(f"CHECKING IF GLITCHED...")
                 pyautogui.sleep(1)
                 l = search(LIGHT)
                 d = search(DARK)
                 if l is not None or d is not None:
+                    print(f"GLITCHING FAILED - WAITING FOR REATTEMPT...")
+                    offsets = [0.01, -0.01, 0.02, -0.02]
+                    offset = random.choice(offsets)
+                    sleep_time += offset
                     pass
                 else:
                     g = search(GLITCHED)
